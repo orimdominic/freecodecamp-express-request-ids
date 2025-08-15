2e900a9b5d71bf326182cf45e8d4f0db54f89afc
Orim Dominic <orimdominic@gmail.com>
2025-08-15 17:12:47 +0100

    feat: use express middlewares

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json(), express.urlencoded({ extended: true }));
 
 app.get("/", function (_, res) {
-  return res.send("OK")
-})
-
+  return res.send("OK");
+});
 
 app.listen(3333, (err) => {
-  if(err) {
-    return console.error(err)
+  if (err) {
+    return console.error(err);
   }
 
-  console.log("Listening on port 3333")
-})
+  console.log("Listening on port 3333");
+});
